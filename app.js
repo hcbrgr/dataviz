7be1acecb8cdc5bb678114982bf48627c324844a
Unknown <maxime.ferraille@gmail.com>
2018-01-30 10:39:02 +0100

    Improve json err

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const Instagram = require('node-instagram').default;
 
 // Create a new instance.
 const instagram = new Instagram({
-  clientId: ' 40918c9d033b4589b849b35c3f56d03d',
-  clientSecret: ' d633cc1333f141f692267e57359f71b7',
-  accessToken: '522066540.1677ed0.1c34121592054c70b3c419646200de4f',
+  clientId: ' 40918c9d036d03d',
+  clientSecret: ' d1333f141f692267e57359f71b7',
+  accessToken: '5220665400.1c34121592054c70b3c419646200de4f',
 });
 
 const redirectUri = 'http://localhost:3000/auth/instagram/callback';
@@ -18,10 +18,14 @@ const app = express();
 app.get('/auth/instagram/callback', async (req, res) => {
   try {
     instagram.get('users/self', (err, data) => {
-        res.json(data);
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(data);
+        }
     });
-  } catch (err) {
-    res.json(err);
+  } catch (error) {
+    res.json(error);
   }
 });
 
